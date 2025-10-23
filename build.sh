30a4eb7b317a89b7e807c393e3ce2d96278d585f
Bianca Pei <biancapei@Carbons-MacBook-Air.local>
2025-10-23 12:48:38 +0800

    Install PHP in build script

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ npm install
 echo "Building assets..."
 npm run build
 
+echo "Installing PHP..."
+apt-get update && apt-get install -y php-cli php-mbstring php-xml php-curl
+
 echo "Installing Composer..."
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
