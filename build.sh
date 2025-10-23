55ed87be6af9f72f525c9208a91e5a2aef4bf227
Bianca Pei <biancapei@Carbons-MacBook-Air.local>
2025-10-23 12:47:41 +0800

    Fix build script to install Composer

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ npm install
 echo "Building assets..."
 npm run build
 
+echo "Installing Composer..."
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
 echo "Installing PHP dependencies..."
 composer install --no-dev --optimize-autoloader
 
