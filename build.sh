db7214b34fd32e6024b22eb1b8ca36850578e8aa
Bianca Pei <biancapei@Carbons-MacBook-Air.local>
2025-10-23 12:57:09 +0800

    Fix build script to use sudo for package installation

--- build.sh
+++ build.sh
@@ -8,14 +8,15 @@ echo "Building assets..."
 npm run build
 
 echo "Installing PHP and Composer..."
-# Install PHP 8.2
-curl -fsSL https://packages.sury.org/php/apt.gpg | apt-key add -
-echo "deb https://packages.sury.org/php/ bullseye main" | tee /etc/apt/sources.list.d/sury-php.list
-apt-get update
-apt-get install -y php8.2-cli php8.2-mbstring php8.2-xml php8.2-curl php8.2-sqlite3 php8.2-zip
+# Install PHP using apt with sudo
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y php8.2-cli php8.2-mbstring php8.2-xml php8.2-curl php8.2-sqlite3 php8.2-zip
 
 # Install Composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 echo "Installing PHP dependencies..."
 composer install --no-dev --optimize-autoloader
